023b9925733e54be0f506ac962e89d3457d221c3
Jagan <110019353+jaganjavid@users.noreply.github.com>
2024-01-23 19:54:24 +0530

    Started Js

---
+++ javascript/1-variable.js
@@ -0,0 +1,36 @@
+
+
+// var, let , const
+
+// var name = "Jagan";
+
+// name = "javid";
+
+// console.log(name);
+
+// INIT Variable
+// var greeting;
+
+// greeting = "Hello";
+
+// console.log(greeting);
+
+// Letter, _, $
+
+// Can not start with number
+// var 5star = "choco";
+// var star5 = "choco";
+
+// console.log(star5);
+
+// Multi word Vars
+
+var firstname = "Jagan"; // normal case
+var firstName = "Jagan 2"; // camel case ***
+var first_name = "Jagan 3"; // snake case
+var FirstName = "Jagan 4" // Pascal case
+var $firstName = "Jagan 5"; // dollar case
+
+// console.log($firstName);
+
+
---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+
+
+// console.log("Hello");
+// console.warn("Please join the class at time");
+// console.error("If you joining the class you are going be in danger");
+
+// let first = window.prompt("Hello are you going to join the class at time");
+// alert(first);
